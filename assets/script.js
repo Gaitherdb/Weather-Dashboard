3d94edfc905d465738c0e9cd480edc89a295ef36
David Gaither <gaitherdb@gmail.com>
2021-07-05 02:36:27 -0400

    add render current day weather

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,23 @@
 var searchFormEl = document.querySelector('#search-form');
 var searchInputVal = document.querySelector('#input-city');
-var currentDay = document.querySelector('#currentDay');
+var currentWeather = document.querySelector('#currentWeather');
 var fiveDayForecast = document.querySelector('#fiveDayForecast');
-var APIKey = "92421b7f2bf12b73f6e7c38295c935c0";
-
-
+const APIKey = "92421b7f2bf12b73f6e7c38295c935c0";
 
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
     var city = searchInputVal.value.trim();
+   
     if (!city) {
         alert('You need a search input value!');
         return;
     }
     else {
-        searchApiCurrentDay(city);
-        currentDay.textContent = '';
         fiveDayForecast.textContent = '';
         searchInputVal.value = '';
+        currentWeather.textContent = '';
+        searchApiCurrentDay(city);
     }
 }
 
@@ -42,13 +41,33 @@ function searchApiCurrentDay(city) {
 
 }
 
-function searchApi5DayForecast(data) {
-   var queryURL = " https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + APIKey;
-   console.log(queryURL);
-}
-// function setLocalStorage() {
+function renderCurrentWeather(data) {
+    console.log(data);
 
-// }
+    var currentDate = getLocalDate(data);
+    var icon = getIcons(data.weather[0].icon);
+    var temp = convertTemp(data.main.temp).toFixed(2);
+    var wind = MStoMPH(data.wind.speed).toFixed(2);
+    var humidity = data.main.humidity;
+    var h1 = document.createElement("h1");
+    var ul = document.createElement("ul");
+    ul.classList.add("todayList");
+    var tempList = document.createElement("li");
+    var windList = document.createElement("li");
+    var humidityList = document.createElement("li");
+    h1.textContent = data.name + " " + currentDate + " " + icon;
+    tempList.textContent = "Temp: " + temp + " ℉";
+    windList.textContent = "Wind: " + wind + " MPH";
+    humidityList.textContent = "Humidity: " + humidity + " %";
+    currentWeather.appendChild(h1);
+    currentWeather.appendChild(ul);
+    ul.appendChild(tempList);
+    ul.appendChild(windList);
+    ul.appendChild(humidityList);
+
+    searchApi5DayForecast(data);
+}
+
 function getLocalDate(data) {
     //gets local time, converts it into miliseconds and adds the product of seconds shifted from UTC and 1000 and converts it to UTC.
     var localDate = new Date((new Date().getTime()) + data.timezone * 1000).toUTCString();
@@ -57,33 +76,82 @@ function getLocalDate(data) {
     date.splice(-2);
     return date;
 }
-
-function convertTemp(K) {
-    return (9 / 5) * (K - 273) + 32;
-}
 function getIcons(icon) {
 
     var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+
     return iconurl;
-    
+
     {/* <div id="icon"><img id="wicon" src="" alt="Weather icon"></div> */ }
 }
+function convertTemp(K) {
+    return (9 / 5) * (K - 273) + 32;
+}
 function MStoMPH(ms) {
-	return ms * 2.236936;
+    return ms * 2.236936;
+}
+function searchApi5DayForecast(currentData) {
+    var queryURL = " https://api.openweathermap.org/data/2.5/onecall?lat=" + currentData.coord.lat + "&lon=" + currentData.coord.lon + "&appid=" + APIKey;
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    render5DayForecast(data);
+                    getUVI(data);
+
+                })
+            } else {
+                alert('Error: ' + response.statusText)
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to OpenWeatherMap.com');
+        })
 }
 
-function renderCurrentWeather(data) {
+function getUVI(data) {
+    var currentUVI = data.current.uvi;
+    var uviList = document.createElement("li");
+    uviList.textContent = "UV Index: " + currentUVI;
+    var ul = document.querySelector(".todayList");
+    ul.appendChild(uviList);
+
+
+    // for (i = 0; i < 5; i++) {
+    //     var forecastUVI = data.daily[i].uvi;
+    //     var div = document.createElement("div");
+    //     fiveDayForecast.appendChild(div);
+    //     div.textContent = forecastUVI;
+
+
+    //     //append each time
+    // }
+    console.log(forecastUVI);
+}
+
+// function setLocalStorage() {
+
+// }
+
+
+
+
+
+
+
+
+// li.setAttribute("data-index", i);
+// li.classList.add("todayList")
+// var h2 = document.createElement("h2");
+// currentWeather.appendChild(h2);
+// h2.textContent = temp;
+// currentWeather.appendChild(h2);
+// h2.textContent = wind + " MPH";
+
+
+function render5DayForecast(data) {
     console.log(data);
-    var currentDate = getLocalDate(data);
-    var temp = convertTemp(data.main.temp).toFixed(2);
-    console.log(temp);
-    var humidity = data.main.humidity;
-    var icon = getIcons(data.weather[0].icon)
-    var wind = MStoMPH(data.wind.speed).toFixed(2);
-
-
-searchApi5DayForecast(data);
-
 
 }
 
